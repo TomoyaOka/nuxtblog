7bef250eaa68c0d1e6dd2f547c1de1fdf40209f7
TomoyaOka <web.toom.oka@gmail.com>
2022-03-14 21:30:27 +0900

    SSG

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ const { API_KEY } = process.env;
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  mode:'universal',
+  ssr: true,
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
